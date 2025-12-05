2da291a183f416125a4feae43029c3333c205098
Justin Hehli <ju.hehli@gmail.com>
2025-12-05 14:29:58 +0100

    refactor dataset prep scripts

--- Segmentation/Util/tif_to_png.py
+++ Segmentation/Util/tif_to_png.py
@@ -1,14 +1,12 @@
 from pathlib import Path
 from PIL import Image
 import shutil
-import re
+import numpy as np
 
-'''
-Converts all .tif images in 'Data/TifImages' to .png format and saves them in 'Data/PngImages', preserving the directory structure.
-The TIF images are assumed to have 1 channel, and the PNG images will also have 1 channel.
-'''
-
-RAW_DATA_DIR = "C:\\Users\\juhe9\\repos\\MasterThesis\\ForkSight\\Segmentation\\Data"
+DATASETS_DIR = "C:\\Users\\juhe9\\repos\\MasterThesis\\ForkSight\\Segmentation\\Data"
+IN_IMAGES = [
+    "Z:\\imcrdata\\2024_Andrea_NBS1\\2024_Andrea_NBS1_R1\\20240523_Andrea_Orange\\LayersData\\highmag\\Tile Set (17)\\Tile_016-003-000000_0-000.tif"
+]
 
 
 def init_folder(folder_path: Path):
@@ -17,45 +15,62 @@ def init_folder(folder_path: Path):
     folder_path.mkdir(parents=True, exist_ok=True)
 
 
+def get_exp_dir_name(img_path: Path) -> str:
+    parts = img_path.parts
+    idx = parts.index("LayersData")
+    return parts[idx - 1].lower()
+
+
+def get_tileset_tile_name(img_path: Path) -> str:
+    tileset = img_path.parent.name
+    tileset = tileset.replace("Tile Set ", "tileset_").replace(
+        " ", "_").replace("(", "").replace(")", "")
+    tile = img_path.stem.replace("-000000_0-000", "").replace("-", "_")
+    return f"{tileset}_{tile}".lower()
+
+
+def get_new_name(img_path: Path) -> str:
+    exp_dir_name = get_exp_dir_name(img_path)
+    tileset_tile_name = get_tileset_tile_name(img_path)
+    return f"{exp_dir_name}_{tileset_tile_name}.png"
+
+
 def save_image_as_png(img: Image.Image, out_dir: Path, out_name: str, resize: tuple = None):
     if resize is not None:
-        img = img.resize(resize, Image.LANCZOS)
+        img = img.resize(resize, Image.Resampling.BILINEAR)
     out_path = out_dir / out_name
     img.save(out_path, format="PNG")
 
 
+def normalize_convert_uint8(img: Image.Image) -> Image.Image:
+    img_arr = np.array(img).astype(np.float32)
+
+    p_low, p_high = np.percentile(img_arr, (1, 99))
+    p_low, p_high = float(p_low), float(p_high)
+    img_arr = (img_arr - p_low) * (255.0 / (p_high - p_low))
+    img_arr = np.clip(img_arr, 0, 255).astype(np.uint8)
+
+    return Image.fromarray(img_arr)
+
+
 def main():
-    raw_data_dir = Path(RAW_DATA_DIR)
-    tif_folder = raw_data_dir / "TifImages"
-    png_folder = raw_data_dir / "images"
-    lowres_png_folder = raw_data_dir / "images_lowres"
+    out_dir_path = Path(DATASETS_DIR) / "images_4096"
+    img_paths = [Path(p) for p in IN_IMAGES]
 
-    if not tif_folder.is_dir():
-        print("Error: inputfolder not found")
-        return
+    init_folder(out_dir_path)
 
-    init_folder(png_folder)
-    init_folder(lowres_png_folder)
-
-    for tif_path in tif_folder.rglob("*.tif"):
+    for img_path in img_paths:
         try:
-            img = Image.open(tif_path)
+            img = Image.open(img_path)
+            img = normalize_convert_uint8(img)
 
-            orig_folder_name = tif_path.parent.name
-            orig_tif_name = tif_path.name
-            new_name = re.sub(r"Site of interest\s*\((\d+)\)",
-                              r"soi_\1", orig_tif_name).lower()
-            new_name = f"{orig_folder_name.lower()}_{new_name}".replace(
-                ".tif", ".png")
+            new_name = get_new_name(img_path)
+            save_image_as_png(img, out_dir_path, new_name)
 
-            save_image_as_png(img, png_folder, new_name)
-            save_image_as_png(img, lowres_png_folder,
-                              new_name, resize=(256, 256))
-            print(
-                f"Converted: {tif_path.relative_to(tif_folder)} → {new_name}")
+            print(f"{new_name}")
 
         except Exception as e:
-            print(f"Failed to convert {tif_path}: {e}")
+            print(f"Failed to convert {img_path}: {e}")
 
 
 if __name__ == "__main__":
